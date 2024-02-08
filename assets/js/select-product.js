00796df7bae5e9c9426d5abb6606683dc08e56d0
Your Name <you@example.com>
2024-02-08 14:24:18 +0100

    first commit

--- assets/js/select-product.js
+++ assets/js/select-product.js
@@ -1,15 +1,27 @@
 /** @type {NodelListOf<HTMImageElement} */
-const productThumbs = document.querySelector('[class^="product__thumb-"]')
+// mettre le querySelectorAll
+const productThumbs = document.querySelectorAll('[class^="product__thumb-"]')
 
 const productDiv = document.querySelector('.product')
 
+// Raccourci jsdoctype
+/** @type {HTMLImageElement} */
+const productMainImg = document.querySelector('.product__main')
+
 // la boucle
-productThumbs.foreach((productThumb, index) => {
+productThumbs.forEach((productThumb, index) => {
   // Ajouter un event handler (handler = traiter) sur le click du productThumb
   productThumb.addEventListener('click', () => {
     // Recuperer le src du productThumb
     const src = productThumb.src
+
     // Donner au src de la productDiv
-    productDiv.src = `product active-${index + 1}`
+    //productDiv.src = `product active-${index + 1}`
+
+    // Donner src en valeur en src de productMainImg
+    productMainImg.src = src
+
+    // Donner au classname de la productDiv
+    productDiv.className = `product active-${index + 1}`
   })
 })
